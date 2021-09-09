f379e0168302c867d66655693d22400cee2341ff
AJ <alexanderjessediaz@gmail.com>
2021-09-09 13:56:07 -0600

    added state for todo input, added todos state

--- App.js
+++ App.js
@@ -1,16 +1,41 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View, TextInput } from 'react-native';
+import React, {useState} from 'react';
+import { StyleSheet, Text, View, TextInput, Pressable } from 'react-native';
 import { NavigationContainer, useNavigation } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Plus from 'react-native-vector-icons/AntDesign';
-import Check from 'react-native-vector-icons/AntDesign'
+import Check from 'react-native-vector-icons/AntDesign';
 
-const TodoInput = () => (
+
+
+
+const TodoInput = () => {
+
+const [text, onChangeText] = useState('')
+
+const [todos, setTodos] = useState([])
+
+const handleSubmit = (text) => {
+   todos.push({todo: text, completed : false})
+   onChangeText('')
+}
+
+
+console.log(todos)
+
+  return (
   <View>
-    <TextInput style={styles.toDoLayout}>Add To Do: </TextInput>
+    <TextInput 
+      style={styles.toDoLayout}
+      value={text}
+      onChangeText={onChangeText}
+      />
+    <Pressable onPress={() => handleSubmit(text)} >
+      <Text>Add Todo</Text>
+    </Pressable>
   </View>
-)
+  
+  )}
 
 const CompletedTodo = () => (
   <View>
